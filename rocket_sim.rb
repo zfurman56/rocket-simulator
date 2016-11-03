13f94ca49e56b4fd175c50bb2acd04dcebc785ee
Zach Furman <zach.furman1@gmail.com>
2016-11-02 17:46:48 -0700

    Added time and step size

--- rocket_sim.rb
+++ rocket_sim.rb
@@ -3,6 +3,11 @@ sim_complete = false
 def get_rocket_commands
 end
 
+step_size = 0.01 #seconds
+time = 0 #seconds
+
 until sim_complete
   rocket_commands = get_rocket_commands
+
+  time += step_size
 end
