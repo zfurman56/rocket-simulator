b16acc6dac863503fa9526ae9c16a8f5d638d3be
Zach Furman <zach.furman1@gmail.com>
2016-11-03 00:42:03 -0700

    Added rocket thrust

--- rocket_sim.rb
+++ rocket_sim.rb
@@ -1,15 +1,25 @@
 require 'matrix'
+require 'interpolator'
 
 sim_complete = false
 
 def get_rocket_commands
 end
 
+# Supply .eng thrust file via command args
+thrust_file = File.open(ARGV[0], 'r')
+thrust_lines = thrust_file.each_line.reject{|line| line[0]==';' }[1..-1]
+raw_thrust = thrust_lines.map{|line| line.chomp.split('   ')[1..2].map(&:to_f)}
+
+# Raw thrust values plus interpolation
+thrust = Interpolator::Table.new raw_thrust.flatten.each_slice(2).map(&:first), raw_thrust.flatten.each_slice(2).map(&:last)
+thrust.extrapolate = false
+
 gravity = Vector[0, -9.8]
 
 mass = 0.595 #kilograms
-position = Vector[0, 100] #meters
-rotation = 0 #degrees
+position = Vector[0, 0] #meters
+rotation = 0 #radians
 velocity = Vector[0, 0] #meters/second
 
 step_size = 0.01 #seconds
@@ -21,6 +31,7 @@ until sim_complete
   rocket_commands = get_rocket_commands
 
   forces += (gravity * mass)
+  forces += (thrust.interpolate(time) * Vector[Math::sin(rotation), Math::cos(rotation)])
 
   velocity += ((forces / mass) * step_size)
   position += (velocity * step_size)
